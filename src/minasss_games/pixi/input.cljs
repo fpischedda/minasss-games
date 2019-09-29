cadba2a8c9eac15e13b304931a91d6a0c5057f54
e_fpischedda <francesco.pischedda@abinsula.com>
2019-09-29 18:36:13 +0200

    add register-keys utility function

--- src/minasss_games/pixi/input.cljs
+++ src/minasss_games/pixi/input.cljs
@@ -4,7 +4,7 @@
   one as a high level interface built on top of the low level one
   which provides higher level abstractions like:
   `(register-keys {:arrow-up ::up :w ::up :arrow-down ::down :s ::down}
-    (fn [native-key-code translated-key-code event-type]
+    (fn [event-type native-key-code translated-key-code]
       (cond
         (and (=translated-key-code :up) (= event-type :key-up)) (jump))))`")
 
@@ -16,15 +16,27 @@
    :key-press "keypress"})
 
 (defn add-key-handler
-  [event-name event-identifier handler]
+  [event-name handler-identifier handler]
   (swap! event-registry_
     (fn [event-map]
       (.addEventListener js/document (get event-mappings event-name) handler)
-      (assoc event-map [event-name event-identifier] handler))))
+      (assoc event-map [event-name handler-identifier] handler))))
 
 (defn remove-key-handler
-  [event-name event-identifier]
+  [event-name handler-identifier]
   (swap! event-registry_
     (fn [event-map]
       (.removeEventListener js/document (get event-mappings event-name))
-      (dissoc event-map [event-name event-identifier]))))
+      (dissoc event-map [event-name handler-identifier]))))
+
+(defn register-keys
+  "Tries to provide a higher level abstraction for key management"
+  [keys-map handler-identifier handler]
+  (let [handler-fn (fn [event-type event]
+                     (let [native (.-key event)
+                           translated (get keys-map native)]
+                       (when (some? translated)
+                         (handler event-type native translated))))]
+    (add-key-handler :key-down handler-identifier (partial handler-fn :key-down))
+    (add-key-handler :key-press handler-identifier (partial handler-fn :key-press))
+    (add-key-handler :key-up handler-identifier (partial handler-fn :key-up))))
