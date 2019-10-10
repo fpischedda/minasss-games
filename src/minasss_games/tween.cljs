589ea1a4acdf5d52cf813bd5f1ce55d1dd0e77fe
Francesco Pischedda <francesco.pischedda@redpineapplemedia.com>
2019-10-10 11:46:25 +0200

    new internal implementation for tweens that will not require local state

--- src/minasss_games/tween.cljs
+++ src/minasss_games/tween.cljs
@@ -4,37 +4,57 @@
   (:require [minasss-games.math :as math]
             [minasss-games.pixi :as pixi]))
 
-(def view-updater-functions_ (atom (list)))
+(def tweens-registry_ (atom (list)))
+
+(defn register-tween
+  [tween]
+  (swap! tweens-registry_ conj tween))
+
+(defmulti updater
+  (fn [tween _delta-time] (:tween tween)))
 
 (defn update-tweens
   [delta-time]
-  (swap! view-updater-functions_
-    (fn [functions]
-      (into () (filter (fn [f] (f delta-time)) functions)))))
+  (swap! tweens-registry_
+    (fn [tweens]
+      (->> tweens
+        (map #(updater % delta-time))
+        (filter some?)
+        (into ())))))
 
-(defn register-tween
-  [f]
-  (swap! view-updater-functions_ conj f))
+(defmethod updater ::move-to
+  [tween delta-time]
+  (let [{:keys [target position target-position direction prev-distance speed]} (:params tween)
+        on-complete (:on-complete tween)
+        new-position {:x (+ (:x position) (* delta-time speed (:x direction)))
+                      :y (+ (:y position) (* delta-time speed (:y direction)))}
+        distance (math/length (math/direction target-position new-position))]
+    (if (> distance prev-distance)
+      ;; lifecycle of this tween finishes here; if on-complete returns
+      ;; something that seems a tween return it so it can be registered
+      (do
+        (pixi/set-position target (:x target-position) (:y target-position))
+        (when (some? on-complete)
+          (let [next-tween (on-complete)]
+            (when (and (map? next-tween) (some? (:tween next-tween)))
+              next-tween))))
+      ;; this tween has not finished yet, in updates the target and
+      ;; prepares the state for next iteration
+      (do
+        (pixi/set-position target (:x new-position) (:y new-position))
+        (update-in tween [:params] assoc :position new-position :prev-distance distance)))))
 
 (defn move-to
   [{:keys [target starting-position target-position speed on-complete]}]
   (let [dir (math/direction target-position starting-position)
         normalized-dir (math/normalize dir)
-        calculated-pos (volatile! starting-position)
-        prev-distance (volatile! (math/length dir))]
+        prev-distance (math/length dir)]
     (register-tween
-      (fn [delta-time]
-        (vswap! calculated-pos (fn [pos]
-                                 {:x (+ (:x pos) (* delta-time speed (:x normalized-dir)))
-                                  :y (+ (:y pos) (* delta-time speed (:y normalized-dir)))}))
-        (let [distance (math/length (math/direction target-position @calculated-pos))]
-          (if (> distance @prev-distance)
-            (do
-              (pixi/set-position target (:x target-position) (:y target-position))
-              (when (some? on-complete)
-                (on-complete))
-              false)
-            (do
-              (vreset! prev-distance distance)
-              (pixi/set-position target (:x @calculated-pos) (:y @calculated-pos))
-              true)))))))
+      {:tween ::move-to
+       :on-complete on-complete
+       :params {:target target
+                :position starting-position
+                :target-position target-position
+                :direction normalized-dir
+                :prev-distance prev-distance
+                :speed speed}})))
