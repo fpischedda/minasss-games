b878bb746f4ad398f20f67d2bafc65e79df870e6
Francesco Pischedda <francesco.pischedda@redpineapplemedia.com>
2019-10-25 18:50:46 +0200

    bump cljsjs/pixi version

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
                  [org.clojure/core.async  "0.4.500"]
-                 [cljsjs/pixi "5.1.2-0"]]
+                 [cljsjs/pixi "5.1.4-0"]]
 
   :plugins [[lein-figwheel "0.5.19"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
